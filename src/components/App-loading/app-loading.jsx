165a4e560aad2bcb98fe5325e220e86eebff2d56
Stanislav Zyatyugin <szyatyugin@e1r5p18.unit.ua>
2020-11-11 11:32:00 +0200

    commit

---
+++ src/components/App-loading/app-loading.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./loading.css";
+
+let Apploading = () => {
+    return (
+        <div className="container">
+            <div className='row justify-content-center'>
+                <div className="loadingio-spinner-spinner-o4nelkcq0s">
+                    <div className="ldio-zwq4lnov3z">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+           
+    );
+};
+export default Apploading;
